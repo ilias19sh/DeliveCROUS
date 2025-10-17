67c7c1a9a845d9361ed60abcf7ab6f15a7dddb35
ilias19sh <hanfaouiilias4@gmail.com>
2025-10-17 14:19:30 +0200

    init of users roots

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { PrismaClient } = require('./generated/prisma');
+const prisma = new PrismaClient()
 const app = express();
 const PORT = 3000
 
@@ -8,6 +10,39 @@ app.use(express.json());
 app.get('/', (req,res) => {
     res.send('hello world')
 })
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post('/users', async (req,res) => {
+    const { name, email, password, role } = req.body; 
+    const user = await prisma.user.create({
+        data : {name, email, password, role}
+    })
+    res.send(`${user.name} bien posté`)
+})
+
+app.put('/users/:id', async (req,res) => {
+    const id = parseInt(req.params.id);
+    const {name, email, password, role} = req.body
+    const user = await prisma.user.update({
+        where : {id},
+        data : {name, email, password, role}
+    })
+
+    res.send(`${name} bien modifié`)
+})
+
+app.delete('/users/:id', async(req,res) => {
+    const id = parseInt(req.params.id);
+    const user = await prisma.user.delete({
+        where : {id}
+    })
+    res.send(`${user.name} bien supprimé`)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
